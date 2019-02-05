865e5c8cef6f7ccacd82e343be53f87322b13e07
Farhan Alfariqi <therealalfariqi@gmail.com>
2019-02-05 13:48:13 +0700

    Update MainManu.java
    Penambahan Komentar pada Source Code Tampilan Menu Utama

--- app/src/main/java/com/example/myapplication/MainManu.java
+++ app/src/main/java/com/example/myapplication/MainManu.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainManu extends AppCompatActivity {
-
+/**Ini merupakan source code untuk menampilkan halaman menu utama aplikasi*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
